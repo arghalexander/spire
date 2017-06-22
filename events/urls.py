707805e88315ed43a30057d84493bcd0c8491368
Douglas <rdouglaslong@gmail.com>
2017-06-22 16:11:42 -0700

    add /event directory above events to prevent collision with pages in cms

--- events/urls.py
+++ events/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
-    url(r'^(?P<slug>[-\w\d]+)/$', views.event_detail, name='event-detail'),
+    url(r'^event/(?P<slug>[-\w\d]+)/$', views.event_detail, name='event-detail'),
 ]
