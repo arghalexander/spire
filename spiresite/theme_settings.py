407ca91b0cab7d6d53d00a4e84da049bd59443f0
Douglas <rdouglaslong@gmail.com>
2017-06-23 13:41:40 -0700

    footer sponsor list

--- spiresite/theme_settings.py
+++ spiresite/theme_settings.py
@@ -1,8 +1,11 @@
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from django.db import models
-
+from wagtail.wagtailadmin.edit_handlers import InlinePanel, FieldPanel, StreamFieldPanel, FieldRowPanel
 from wagtail.wagtailadmin.edit_handlers import TabbedInterface, ObjectList
-
+from wagtail.wagtailcore.models import Page, Orderable
+from modelcluster.fields import ParentalKey
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailcore.fields import RichTextField
 
 @register_setting(icon='link')
 class SocialMediaSettings(BaseSetting):
@@ -21,3 +24,24 @@ class SocialMediaSettings(BaseSetting):
 class ThemeSettings(BaseSetting):
     contact_form_email = models.EmailField(max_length=255, help_text='Address that contact form submissions will be sent to')
     contact_form_message = models.CharField(max_length=255)
+
+    footer = RichTextField(blank=True)
+
+
+
+class CoreSponsors(Orderable):
+    setting = models.ForeignKey(ThemeSettings, related_name='core_sponsors')
+    title =  models.CharField(max_length=255)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='sponsor_logo'
+    )
+
+    panels = [
+        FieldPanel('title'),
+        ImageChooserPanel('image'),
+    ]
+
