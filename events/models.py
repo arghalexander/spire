e8fa79bbe0994cefd8e41c6f3de2e7848998595a
Douglas <rdouglaslong@gmail.com>
2017-06-27 17:54:49 -0700

    add capacity to events

--- events/models.py
+++ events/models.py
@@ -42,6 +42,7 @@ class Event(index.Indexed,ClusterableModel):
 	all_day = 								models.BooleanField()
 	start = 								models.DateTimeField()
 	end =									models.DateTimeField()
+	capacity = 								models.IntegerField(blank=True,null=True)
 	address = 								models.CharField(max_length=250, blank=True, null=True)
 	location = 								models.CharField(max_length=255, blank=True, null=True)
 	description = 							RichTextField()
