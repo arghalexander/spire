fc82503337a7988d368a367b63eb8f5e58c65565
Douglas <rdouglaslong@gmail.com>
2017-07-04 18:02:25 -0700

    csrf exempt on notes

--- spire/settings/base.py
+++ spire/settings/base.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        #'rest_framework.permissions.IsAuthenticated',
         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
         'rest_framework.permissions.AllowAny',
     ],
