6335236af5d0738e1667dd8dd0d447b28706be1d
Douglas <rdouglaslong@gmail.com>
2017-07-04 18:01:55 -0700

    csrf exempt on notes

--- spire/settings/base.py
+++ spire/settings/base.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        #'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework_filters.backends.DjangoFilterBackend',
