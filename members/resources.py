57c1e49897bc7ac7d46cb6f82bf2336fd09d6461
Douglas <rdouglaslong@gmail.com>
2017-07-07 13:52:17 -0700

    member resource

--- members/resources.py
+++ members/resources.py
@@ -1,14 +1,15 @@
 from import_export import resources
-from import_export.widgets import ForeignKeyWidget
+from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 from import_export import fields
 
 from django.contrib.auth.models import User
 
-from .models import Member, MembershipLevel
+from .models import Member, MembershipLevel, MemberRegion
 
 class MemberResource(resources.ModelResource):
     user = fields.Field( column_name='user',attribute='user',widget=ForeignKeyWidget(User, 'username'))
     membership_level = fields.Field( column_name='membership_level',attribute='membership_level',widget=ForeignKeyWidget(MembershipLevel, 'level'))
+    region = fields.Field(widget=ManyToManyWidget(MemberRegion))
 
     class Meta:
         model = Member
