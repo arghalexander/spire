e8b0f9f6a085ba5077154f0b0d2ae99cc6a1f852
Douglas <rdouglaslong@gmail.com>
2017-07-07 16:40:53 -0700

    member admin list display

--- members/admin.py
+++ members/admin.py
@@ -29,13 +29,24 @@ class NoteInline(admin.TabularInline):
 class MemberAdmin(ImportExportModelAdmin):
 	resource_class = MemberResource
 	search_fields = ['user__username']
-	list_display = ['user__username', 'user__first_name', 'user__last_name', 'region','membership_level', 'membership_expiration']
+	list_display = ['user', 'get_first_name', 'get_last_name', 'membership_level', 'membership_expiration']
 	inlines = [
 		MemberEducationInline,
 		AddressInline,
 		NoteInline,
 		ProfesionalInfoInline
 	]
+
+	def get_first_name(self, obj):
+		return obj.user.first_name
+	get_first_name.short_description = 'First Name'
+	get_first_name.admin_order_field = 'user__first_name'
+
+	def get_last_name(self, obj):
+		return obj.user.last_name
+	get_last_name.short_description = 'Last Name'
+	get_last_name.admin_order_field = 'user__last_name'
+
 
 
 @admin.register(MemberNote)
