764efce509e649704afbe6c3feec65293b996f84
Douglas <rdouglaslong@gmail.com>
2017-07-07 18:48:09 -0700

    many to many widget

--- members/resources.py
+++ members/resources.py
@@ -9,8 +9,9 @@ from .models import Member, MembershipLevel, MemberRegion
 class MemberResource(resources.ModelResource):
     user = fields.Field( column_name='user',attribute='user',widget=ForeignKeyWidget(User, 'username'))
     membership_level = fields.Field( column_name='membership_level',attribute='membership_level',widget=ForeignKeyWidget(MembershipLevel, 'level'))
-    region = fields.Field(widget=ManyToManyWidget(MemberRegion, field='region'))
+    region = fields.Field(widget=ManyToManyWidget(MemberRegion))
 
     class Meta:
         model = Member
+		import_field =         
         fields = ('id','user','work_phone','mobile_phone', 'membership_level', 'membership_expiration')
