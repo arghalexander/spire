b6226e7f2341a17b12b35364843f69a8d54e72fd
Douglas <rdouglaslong@gmail.com>
2017-07-17 15:09:55 -0700

    job board fix

--- spiresite/urls.py
+++ spiresite/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^jobs/new/$', views.job_create, name='job-create'),
-    url(r'^jobs/(?P<job_id>[1-9]+)/$', views.job_detail, name='job-detail'),
+    url(r'^jobs/(?P<job_id>[0-9]+)/$', views.job_detail, name='job-detail'),
 ]
