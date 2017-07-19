4bfcd46ea5ff466e4891419892feec08b3706a72
Douglas <rdouglaslong@gmail.com>
2017-07-18 18:13:23 -0700

    update import export

--- spire/resources.py
+++ spire/resources.py
@@ -2,13 +2,15 @@ from import_export import resources
 from import_export.widgets import ForeignKeyWidget
 from import_export import fields
 from django.contrib.auth.models import User
-
+from import_export.widgets import DateWidget
 
 class UserResource(resources.ModelResource):
+    date_joined = fields.Field(attribute='date_joined', column_name="date_joined", widget=DateWidget())
+
     class Meta:
         model = User
         import_id_fields = ('username',)
         fields = ('id','username','email','first_name','last_name','date_joined')
-        widgets = {
+        #widgets = {
             #'date_joined': {'format': "%Y-%m-%dT%H:%M:%S"},
-        }
+        #}
