6d7c103e78df8722d1ba23899643a42c69dbc946
Douglas <rdouglaslong@gmail.com>
2017-07-20 12:17:09 -0700

    member directory update

--- spire/settings/base.py
+++ spire/settings/base.py
@@ -158,6 +158,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
         #'rest_framework.permissions.AllowAny',
     ],
