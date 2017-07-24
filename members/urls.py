b5fc8942430c48951a36cd1314f834c9c4a48c08
Douglas <rdouglaslong@gmail.com>
2017-07-24 11:43:39 -0700

    remove memeber index, it was replcaed by member directory

--- members/urls.py
+++ members/urls.py
@@ -4,13 +4,9 @@ from . import views
 
 app_name = 'members'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-
+   
     url(r'^profile/(?P<member_id>[0-9]+)/$', views.member_profile, name='member-profile-lookup'),
-    
-  
  	url(r'^create/$', views.member_create, name='member-create'),   
-
     url(r'^profile/$', views.my_profile, name='member-profile'),
    	url(r'^profile/edit/$', views.my_profile_edit, name='member-profile-edit'),
 
