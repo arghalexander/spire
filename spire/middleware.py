e77ce62942cbd5e9f7dcfeb57998b06616e58fb2
Douglas <rdouglaslong@gmail.com>
2017-07-24 21:44:26 -0700

    student signup

--- spire/middleware.py
+++ spire/middleware.py
@@ -18,7 +18,7 @@ def CreateMembershipMiddleware(get_response):
 					return redirect('members:member-create')
 
 				#student memebrs might have empty profile, test region
-				if member.region == None:
+				if not member.region:
 					messages.warning(request, 'Please complete your member profile')
 					return redirect('members:member-edit')
 
